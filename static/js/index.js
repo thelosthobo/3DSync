883a6d8f365962f552170123b68d86a6ace7157b
Aric Huang <arichuang@gmail.com>
2022-06-03 23:38:51 -0700

    no base64?

--- static/js/index.js
+++ static/js/index.js
@@ -24,7 +24,7 @@ $(function(){
         localStorage.setItem('token', token);
         let challengeCode = [...Array(100)].map(i=>(~~(Math.random()*36)).toString(36)).join('');
         console.log(challengeCode);
-        challengeCode = btoa(challengeCode);
+//         challengeCode = btoa(challengeCode);
         window.location.href = "https://www.dropbox.com/oauth2/authorize?client_id=3x8ipjhtplvcoba&response_type=code&token_access_type=offline&code_challenge=" + challengeCode + "&code_challenge_method=plain&redirect_uri=https://kyraminol.github.io/3DSync/&state=" + token;
     });
 
